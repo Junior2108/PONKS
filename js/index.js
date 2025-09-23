5881c0c5a9d679dd8d81dc4fc9e3f943645f0d7d
Mister_June <58920829+Junior2108@users.noreply.github.com>
2025-09-23 13:27:40 +0200

    Ajout js photo cliquable

--- js/index.js
+++ js/index.js
@@ -6,3 +6,27 @@ miniatures.forEach(mini => {
   imagePrincipale.src = mini.src.replace("100", "500"); 
     });
     });
+
+
+const thumbnail = document.querySelector('.thumbnail');
+    const overlay = document.getElementById('overlay');
+    const overlayImg = document.getElementById('overlayImg');
+    const closeBtn = document.getElementById('closeBtn');
+
+    // Quand on clique sur la miniature → afficher l'image
+    thumbnail.addEventListener('click', () => {
+      overlay.style.display = 'flex';
+      overlayImg.src = thumbnail.src; // on reprend la même image
+    });
+
+    // Fermer en cliquant sur X
+    closeBtn.addEventListener('click', () => {
+      overlay.style.display = 'none';
+    });
+
+    // Fermer en cliquant en dehors de l'image
+    overlay.addEventListener('click', (e) => {
+      if (e.target === overlay) {
+        overlay.style.display = 'none';
+      }
+    });
